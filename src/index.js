541a9360f7a886568de8415b3b9943dec33822bc
winkerVSbecks <varunvachhar@gmail.com>
2021-11-27 19:15:39 -0500

    rangeele-yantra

--- src/index.js
+++ src/index.js
@@ -1,29 +1,14 @@
-// these are the variables you can use as inputs to your algorithms
-console.log(fxhash)   // the 64 chars hex number fed to your algorithm
-console.log(fxrand()) // deterministic PRNG function, use it instead of Math.random()
+import { initSketch } from './sketch';
 
-// note about the fxrand() function 
-// when the "fxhash" is always the same, it will generate the same sequence of
-// pseudo random numbers, always
+// See if tokenData exists from ArtBlocks input
+// It may come in as object or string depending on context
+let hash = typeof tokenData !== 'undefined' ? tokenData.hash : window.fxhash;
 
-//----------------------
-// defining features
-//----------------------
-// You can define some token features by populating the $fxhashFeatures property
-// of the window object.
-// More about it in the guide, section features:
-// [https://fxhash.xyz/articles/guide-mint-generative-token#features]
-//
-// window.$fxhashFeatures = {
-//   "Background": "Black",
-//   "Number of lines": 10,
-//   "Inverted": true
-// }
+export default (() => {
+  let W = window,
+    D = document;
+  const canvas = D.body.appendChild(D.createElement('canvas'));
+  const context = canvas.getContext('2d');
 
-// this code writes the values to the DOM as an example
-const container = document.createElement("div")
-container.innerText = `
-  random hash: ${fxhash}\n
-  some pseudo random values: [ ${fxrand()}, ${fxrand()}, ${fxrand()}, ${fxrand()}, ${fxrand()},... ]\n
-`
-document.body.prepend(container)
+  initSketch({ hash, canvas, context });
+})();
